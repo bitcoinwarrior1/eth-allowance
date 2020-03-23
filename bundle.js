018c06d9dff8de28126082958318b5eca3b6f5eb
James Sangalli <j.l.sangalli@gmail.com>
2020-03-23 14:07:04 +1100

    get accounts properly and clean  up header

--- bundle.js
+++ bundle.js
@@ -3,6 +3,7 @@ let Web3 = require('web3');
 let web3 = new Web3(Web3.givenProvider);
 let request = require('superagent');
 const approvalHash = "0x095ea7b3";
+const unlimitedAllowance = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
 const approvalABI = [
     {
         "constant": false,
@@ -32,17 +33,17 @@ const approvalABI = [
     }
 ];
 
-document.addEventListener("DOMContentLoaded", () => {
+$(() => {
 
     web3.eth.requestAccounts().then((accounts) => {
-        init();
+        init(accounts[0]);
     });
 
-    function init() {
+    function init(account) {
         web3.eth.getChainId().then((chainId) => {
             return chainId;
-        }).then((chainId, address) => {
-            let query = getQuery(chainId, address);
+        }).then((chainId) => {
+            let query = getQuery(chainId, account);
             getApproveTransactions(query, (txs) => {
                 // display the logic
                 console.log(txs);
@@ -56,15 +57,15 @@ document.addEventListener("DOMContentLoaded", () => {
     function getQuery(chainId, address) {
         switch (chainId) {
             case "1":
-                return "http://api.etherscan.io/api?module=account&action=txlist&address=" + address;
+                return "https://api.etherscan.io/api?module=account&action=txlist&address=" + address;
             case "3":
-                return "http://ropsten.etherscan.io/api?module=account&action=txlist&address=" + address;
+                return "https://ropsten.etherscan.io/api?module=account&action=txlist&address=" + address;
             case "4":
-                return "http://rinkeby.etherscan.io/api?module=account&action=txlist&address=" + address;
+                return "https://rinkeby.etherscan.io/api?module=account&action=txlist&address=" + address;
             case "42":
-                return "http://kovan.etherscan.io/api?module=account&action=txlist&address=" + address;
+                return "https://kovan.etherscan.io/api?module=account&action=txlist&address=" + address;
             default:
-                return "http://api.etherscan.io/api?module=account&action=txlist&address=" + address;
+                return "https://api.etherscan.io/api?module=account&action=txlist&address=" + address;
         }
     }
 
@@ -72,47 +73,46 @@ document.addEventListener("DOMContentLoaded", () => {
         request.get(query, (err, data) => {
             if(err) throw err;
             let approveTransactions = [];
-            for(let tx of data.result) {
+            let dataObj = JSON.parse(data.text).result;
+            for(let tx of dataObj) {
                 if(tx.input.includes(approvalHash)) {
-                    // 0x095ea7b3000000000000000000000000e5a69d694d0df71a5eea63432ee74f2c4465a56
-                    // fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
                     let approveObj = {};
-                    approveObj.contract = tx.to;
-                    approveObj.approved = "0x" + tx.input.substring(34, 73);
-                    approveObj.allowance = tx.input.substring(74);
-                    approveTransactions.push(approveObj);
+                    approveObj.contract = web3.utils.toChecksumAddress(tx.to);
+                    approveObj.approved = web3.utils.toChecksumAddress("0x" + tx.input.substring(34, 74));
+                    let allowance = tx.input.substring(74);
+                    if(allowance.includes(unlimitedAllowance)) {
+                        approveObj.allowance = "unlimited";
+                    } else {
+                        approveObj.allowance = "some";
+                    }
+                    if(parseInt(allowance, 16) !== 0) {
+                        approveTransactions.push(approveObj);
+                    }
                 }
             }
             cb(approveTransactions);
         });
     }
 
-
-    // <div class="grid-container">
-    //     <div class="grid-items">0xEdd6D7ba0FF9f4bC501a12529cb736CA76A4fe7e</div>
-    //     <div class="grid-items">0xEdd6D7ba0FF9f4bC501a12529cb736CA76A4fe7e</div>
-    //     <div class="grid-items">Unlimited <button class="btn btn-warning">Revoke</button></div>
-    // </div>
-
     function buildResults(txs) {
-        let parentElement = document.getElementById("results");
+        let parentElement = $('#results');
         for(let index in txs) {
             parentElement.append(`
                 <div class="grid-container">
-                    <div class="grid-item">${txs[index].contract}</div>
-                    <div class="grid-item">${txs[index].approved}</div>
-                    <div class="grid-item">${txs[index].allowance}<button id="revoke${index}"</button></div>
+                    <div class="grid-items">${txs[index].contract}</div>
+                    <div class="grid-items">${txs[index].approved}</div>
+                    <div class="grid-items">${txs[index].allowance}<button class="btn btn-primary" id="revoke${index}">Revoke</button></div>
                 </div>
                 `);
-            setRevokeButtonClick(txs[index], "revoke" + index);
+            setRevokeButtonClick(txs[index], "#revoke" + index);
         }
     }
 
     function setRevokeButtonClick(tx, id) {
-        document.getElementById(id).onclick((ev) => {
+        $(id).click(() => {
             // set the contract and make an approve transaction with a zero allowance
             let contract = new web3.eth.Contract(approvalABI, tx.contract);
-            contract.methods.approve([tx.approved, 0]).send().then((receipt) => {
+            contract.methods.approve(tx.approved, 0).send().then((receipt) => {
                 alert("revoked: " + receipt);
             }).catch((err) => {
                 alert("failed: " + err);
