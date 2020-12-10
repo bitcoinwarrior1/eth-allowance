b9e52e9683ac839de5b85023030b67a7af5fed9c
James Sangalli <j.l.sangalli@gmail.com>
2020-12-10 15:55:48 +1100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# eth-allowance
-Find all the address which can spend your crypto! 
--- dapps.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "compound": "https://app.compound.finance/#",
-    "aave": "https://aave.com/",
-    "uniswap": "https://uniswap.org/",
-    "nest": "https://nestdapp.io/",
-    "cofi": "https://cofix.io/"
-}
