88993cd22dded48813012085d1179871c3899101
James Sangalli <jamessangalli@MacBook-Pro-66.local>
2022-03-03 12:49:35 +1100

    add more popular dapps #35

--- src/helpers/dapps.js
+++ src/helpers/dapps.js
@@ -3,5 +3,17 @@ module.exports = {
     "aave": "https://aave.com/",
     "uniswap": "https://uniswap.org/",
     "nest": "https://nestdapp.io/",
-    "cofi": "https://cofix.io/"
+    "cofi": "https://cofix.io/",
+    "chainlink": "https://chain.link/",
+    "USD Coin": "https://www.centre.io/",
+    "Tether USD": "https://tether.to/en/",
+    "True USD": "https://www.trueusd.com/",
+    "cryptokitties": "https://www.cryptokitties.co/",
+    "kyber network crystal": "https://kyber.network/",
+    "WETH": "https://james-sangalli.github.io/weth-made-easy/",
+    "Basic Attention Token": "https://basicattentiontoken.org/",
+    "dai stablecoin": "https://makerdao.com/en/",
+    "matic": "https://polygon.technology/",
+    "LooksRare": "https://looksrare.org/",
+    "Fantom token": "https://fantom.foundation/"
 }
